e43c393d6482e4cb4eb377b198c040dd64630f56
AnamZ78 <anamzahid126@gmail.com>
2025-06-15 17:58:59 +0530

    done

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Product, Contact
+from .models import Product, Contact, Orders
 admin.site.register(Product)
 admin.site.register(Contact)
+admin.site.register(Orders)
